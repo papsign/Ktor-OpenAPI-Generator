9508de3ba304400c54d2e472ba95c13a54650882
Frederic Nieto <frederic.artus.nieto@gmail.com>
2019-10-15 14:24:38 +0200

    added routing hook for openapigen

--- src/main/kotlin/com/papsign/ktor/openapigen/route/RouteConfig.kt
+++ src/main/kotlin/com/papsign/ktor/openapigen/route/RouteConfig.kt
@@ -4,6 +4,7 @@ import com.papsign.ktor.openapigen.OpenAPIGen
 import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
 import io.ktor.application.Application
 import io.ktor.application.feature
+import io.ktor.routing.Routing
 import io.ktor.routing.routing
 import io.ktor.util.pipeline.ContextDsl
 
@@ -11,9 +12,17 @@ import io.ktor.util.pipeline.ContextDsl
 fun Application.apiRouting(config: NormalOpenAPIRoute.() -> Unit) {
     routing {
         NormalOpenAPIRoute(
-            this,
-            application.feature(OpenAPIGen).globalModuleProvider
+                this,
+                application.feature(OpenAPIGen).globalModuleProvider
         ).apply(config)
     }
+}
+
+@ContextDsl
+fun Routing.apiRouting(config: NormalOpenAPIRoute.() -> Unit) {
+    NormalOpenAPIRoute(
+            this,
+            application.feature(OpenAPIGen).globalModuleProvider
+    ).apply(config)
 }
 
