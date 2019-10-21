1f8762e013ae87f16d40f4f046936864328bb3a5
Frederic Nieto <frederic.artus.nieto@gmail.com>
2019-10-21 17:04:56 +0200

    improved validation handler

---
+++ src/main/kotlin/com/papsign/ktor/openapigen/validators/defaults/LowerCase.kt
@@ -0,0 +1,13 @@
+package com.papsign.ktor.openapigen.validators.defaults
+
+import com.papsign.ktor.openapigen.validators.util.AValidator
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.PROPERTY)
+annotation class LowerCase
+
+object LowerCaseValidator : AValidator<String, LowerCase>(String::class, LowerCase::class) {
+    override fun validate(subject: String?, annotation: LowerCase): String? {
+        return subject?.toLowerCase()
+    }
+}
