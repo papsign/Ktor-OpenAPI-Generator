7dc9db269a86d57ffdc0fabbf840cbda21562f7d
Bryan Herbst <bryan.c.herbst@gmail.com>
2020-05-21 13:29:39 -0500

    Only allow descriptions on properties, add doc comment

--- src/main/kotlin/com/papsign/ktor/openapigen/annotations/properties/description/Description.kt
+++ src/main/kotlin/com/papsign/ktor/openapigen/annotations/properties/description/Description.kt
@@ -2,6 +2,9 @@ package com.papsign.ktor.openapigen.annotations.properties.description
 
 import com.papsign.ktor.openapigen.schema.processor.SchemaProcessorAnnotation
 
-@Target(AnnotationTarget.TYPE, AnnotationTarget.PROPERTY)
+/**
+ * Property annotation for providing a description of a schema model property
+ */
+@Target(AnnotationTarget.PROPERTY)
 @SchemaProcessorAnnotation(DescriptionProcessor::class)
 annotation class Description(val value: String)
