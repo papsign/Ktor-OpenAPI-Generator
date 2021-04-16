fb268b9006988a8fd2b6dbb0180e2126f26b93d6
Emanuel Moecklin <emanuel.moecklin@gmail.com>
2021-04-16 17:10:11 -0400

    apply the same changes to the secure routes: add exceptions as route parameters so they can be attached to a route directly without the need to wrap it in a throws {} expression

--- src/main/kotlin/com/papsign/ktor/openapigen/route/path/auth/Functions.kt
+++ src/main/kotlin/com/papsign/ktor/openapigen/route/path/auth/Functions.kt
@@ -1,9 +1,12 @@
 package com.papsign.ktor.openapigen.route.path.auth
 
+import com.papsign.ktor.openapigen.APIException
 import com.papsign.ktor.openapigen.modules.RouteOpenAPIModule
 import com.papsign.ktor.openapigen.route.method
+import com.papsign.ktor.openapigen.route.path.normal.route
 import com.papsign.ktor.openapigen.route.preHandle
 import com.papsign.ktor.openapigen.route.response.OpenAPIPipelineAuthContext
+import com.papsign.ktor.openapigen.route.throws
 import io.ktor.http.HttpMethod
 import io.ktor.util.pipeline.ContextDsl
 import kotlin.reflect.full.starProjectedType
@@ -12,69 +15,79 @@ import kotlin.reflect.typeOf
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.get(
     vararg modules: RouteOpenAPIModule,
+    exceptions: Collection<APIException<*, *>>? = null,
     example: TResponse? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams) -> Unit
-) = route(HttpMethod.Get, modules, example, body)
+) = route(HttpMethod.Get, modules, exceptions, example, body)
 
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, reified TRequest : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.post(
     vararg modules: RouteOpenAPIModule,
+    exceptions: Collection<APIException<*, *>>? = null,
     exampleResponse: TResponse? = null,
     exampleRequest: TRequest? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams, TRequest) -> Unit
-) = route(HttpMethod.Post, modules, exampleResponse, exampleRequest, body)
+) = route(HttpMethod.Post, modules, exceptions, exampleResponse, exampleRequest, body)
 
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, reified TRequest : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.put(
     vararg modules: RouteOpenAPIModule,
+    exceptions: Collection<APIException<*, *>>? = null,
     exampleResponse: TResponse? = null,
     exampleRequest: TRequest? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams, TRequest) -> Unit
-) = route(HttpMethod.Put, modules, exampleResponse, exampleRequest, body)
+) = route(HttpMethod.Put, modules, exceptions, exampleResponse, exampleRequest, body)
 
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, reified TRequest : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.patch(
     vararg modules: RouteOpenAPIModule,
+    exceptions: Collection<APIException<*, *>>? = null,
     exampleResponse: TResponse? = null,
     exampleRequest: TRequest? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams, TRequest) -> Unit
-) = route(HttpMethod.Patch, modules, exampleResponse, exampleRequest, body)
+) = route(HttpMethod.Patch, modules, exceptions, exampleResponse, exampleRequest, body)
 
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.delete(
     vararg modules: RouteOpenAPIModule,
+    exceptions: Collection<APIException<*, *>>? = null,
     example: TResponse? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams) -> Unit
-) = route(HttpMethod.Delete, modules, example, body)
+) = route(HttpMethod.Delete, modules, exceptions, example, body)
 
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.head(
     vararg modules: RouteOpenAPIModule,
+    exceptions: Collection<APIException<*, *>>? = null,
     example: TResponse? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams) -> Unit
-) = route(HttpMethod.Head, modules, example, body)
+) = route(HttpMethod.Head, modules, exceptions, example, body)
 
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, reified TRequest : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.route(
     method: HttpMethod,
     modules: Array<out RouteOpenAPIModule>,
+    exceptions: Collection<APIException<*, *>>? = null,
     exampleResponse: TResponse? = null,
     exampleRequest: TRequest? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams, TRequest) -> Unit
 ) {
-    method(method).apply { modules.forEach { provider.registerModule(it, it::class.starProjectedType) } }
-        .handle(exampleResponse, exampleRequest, body)
+    modules.forEach { provider.registerModule(it, it::class.starProjectedType) }
+    val route = if (exceptions == null) this else throws(*exceptions.toTypedArray())
+    route.method(method).handle(exampleResponse, exampleRequest, body)
 }
 
 @ContextDsl
 inline fun <reified TParams : Any, reified TResponse : Any, TAuth> OpenAPIAuthenticatedRoute<TAuth>.route(
     method: HttpMethod,
     modules: Array<out RouteOpenAPIModule>,
+    exceptions: Collection<APIException<*, *>>? = null,
     exampleResponse: TResponse? = null,
     noinline body: suspend OpenAPIPipelineAuthContext<TAuth, TResponse>.(TParams) -> Unit
 ) {
-    method(method).apply { modules.forEach { provider.registerModule(it, it::class.starProjectedType) } }
-        .handle(exampleResponse, body)
+    modules.forEach { provider.registerModule(it, it::class.starProjectedType) }
+    val route = if (exceptions == null) this else throws(*exceptions.toTypedArray())
+    route.method(method).handle(exampleResponse, body)
 }
 
 @ContextDsl
